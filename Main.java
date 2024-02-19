a21d27a4152e4626395abcb788ec1f160af188be
felmaralfonso <f.alfonso.62200.dc@umindanao.edu.ph>
2024-02-19 14:17:26 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Activity3TemperatureConverter;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        double celcius = 20;
+        
+        double farenheit = (celcius * 9/ 5) + 32;
+        
+        System.out.println(farenheit);
+    }
+    
+}
